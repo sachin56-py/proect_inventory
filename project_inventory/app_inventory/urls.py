0ac79c5e89ab5992da7c2b6922bbdbc6c1dbb3ba
sachinmnm10 <sachinmnm10@gmail.com>
2022-12-17 19:47:40 +0545

    form created

--- project_inventory/app_inventory/urls.py
+++ project_inventory/app_inventory/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # items
     path("items/", views.item_index, name="items.index"),
     path("items/show/", views.item_show, name="items.show"),
     path("items/create/", views.item_create, name="items.create"),
-    path("items/edit/", views.item_edit, name="items.edit")
+    path("items/edit/", views.item_edit, name="items.edit"),
+
+    # users
+    path("users/login/", views.user_login, name="users.login"),
+    path("users/register/", views.user_register, name="users.register"),
 
 ]
--- project_inventory/app_inventory/views.py
+++ project_inventory/app_inventory/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import ItemCreateForm, UserLoginForm, UserRegisterForm
 
 # Create your views here.
 def item_index(request):
@@ -8,7 +9,19 @@ def item_show(request):
     return render(request, "items/show.html")
 
 def item_edit(request):
-    return render(request, "items/edit.html")
+    return render(request, "items/edit.htsml")
 
 def item_create(request):
-    return render(request, "items/create.html")
+    form = ItemCreateForm()
+    context = {"form": form}     # could not understand this code
+    return render(request, "items/create.html", context)
+
+def user_login(request):
+    form = UserLoginForm()
+    context = {"form": form}
+    return render(request, "users/login.html", context)
+
+def user_register(request):
+    form = UserRegisterForm()
+    context = {"form": form}
+    return render(request, "users/register.html", context)
